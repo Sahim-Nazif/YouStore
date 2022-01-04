324c3df30015e4ba63aaa9fa1394f73da63bed74
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-01-03 19:25:00 -0500

    added contact, home and about pages componenets

---
+++ client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+const ContactPage = () => {
+    return (
+        <Typography>
+            Contact page
+        </Typography>
+    )
+}
+
+export default ContactPage
