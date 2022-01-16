8bb165bde895b0dd07f980e9f02f34d1015b2cf5
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-01-16 17:06:04 -0500

    created method to retrieve items in basket

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController:BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            this._context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket=await _context.Baskets
+                        .Include(i=>i.Items)
+                        .ThenInclude(p=>p.Product)
+                        .FirstOrDefaultAsync(x=>x.BuyerId==Request.Cookies["BuyerId"]);
+
+            if(basket==null){
+                return NotFound();
+            }
+
+            return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>AddItem(int productId, int quantity)
+        {
+        
+            return StatusCode(201);
+        }
+    }
+}
