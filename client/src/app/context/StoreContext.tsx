a036b7f5baf1b336e71b7864012c5645e136ce70
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-01-19 21:06:03 -0500

    created store context & added index to be available to all

---
+++ client/src/app/context/StoreContext.tsx
@@ -0,0 +1,49 @@
+import {Basket} from '../layout/models/basket'
+import { createContext, PropsWithChildren, useContext, useState } from 'react';
+
+
+interface StoreContextValue {
+
+    basket: Basket | null;
+    setBasket :(basekt: Basket) => void;
+    removeItem :(productId: number, quantity: number)=> void;
+}
+
+export const StoreContext=createContext<StoreContextValue | undefined>(undefined)
+
+
+export const useStoreContext=()=>{
+
+    const context= useContext(StoreContext)
+
+    if (context===undefined) {
+        throw Error ('We do not seem to be inside the provider')
+    }
+    return context
+}
+
+export const StoreProvider=({children} :PropsWithChildren<any>)=>{
+
+    const [basket, setBasket]= useState<Basket | null>(null)
+
+    const removeItem=(productId:number, quantity:number)=>{
+        if (!basket) return;
+
+        const items=[...basket.items]
+        const itemIndex=items.findIndex(i=>i.productId===productId)
+        if (itemIndex>=0) {
+            items[itemIndex].quantity -= quantity
+            if(items[itemIndex].quantity===0) items.splice(itemIndex, 1)
+
+            setBasket(prevState=>{
+                return {...prevState!, items}
+            })
+        }
+    }
+    return (
+
+        <StoreContext.Provider value={{basket, setBasket, removeItem}}>
+                {children}
+        </StoreContext.Provider>
+    )
+}
