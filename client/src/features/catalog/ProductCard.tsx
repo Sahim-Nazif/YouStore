2544ca4927a1a7ac3d2cb03ea507e4e471524ab6
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-02-17 21:50:06 -0500

    useAppSelector in productCard

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -5,8 +5,8 @@ import { useState } from 'react'
 import agent from '../../app/api/agent'
 import { LoadingButton } from '@mui/lab'
 import { useStoreContext } from '../../app/context/StoreContext'
-import { useAppDispatch } from '../../app/store/configureStore'
-import { setBasket } from '../basket/basketSlice'
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore'
+import { addBasketItemAsync, setBasket } from '../basket/basketSlice'
 
 interface Props {
 
@@ -16,16 +16,9 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
 
-    const [loading, setLoading]=useState(false)
+     const {status}= useAppSelector(state=>state.basket)
     const dispatch=useAppDispatch()
 
-    const handleAddItem=(productId:number)=>{
-        setLoading(true)
-        agent.Basket.addItem(productId)
-                    .then(basket=> dispatch(setBasket(basket)))
-                    .catch(error=>console.log(error))
-                    .finally(()=>setLoading(false))
-    }
     return (
         <>
 
@@ -56,7 +49,7 @@ const ProductCard = ({ product }: Props) => {
                   
                 </CardContent>
                 <CardActions>
-                    <LoadingButton loading={loading} onClick={()=>handleAddItem(product.id)} size="small">Add to Card</LoadingButton>
+                    <LoadingButton loading={status.includes('pendingAddItem' + product.id)} onClick={()=>dispatch(addBasketItemAsync({productId:product.id}))} size="small">Add to Card</LoadingButton>
                     <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
                 </CardActions>
             </Card>
