de9d0347f10874a398619cfd0afc56896022a43b
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-02-21 19:53:39 -0500

    error fixed for removing an item from cart

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -8,38 +8,15 @@ import { LoadingButton } from '@mui/lab';
 import BasketSummary from './BasketSummary'
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
-import { removeItem, setBasket } from './basketSlice';
+import {addBasketItemAsync, removeBasketItemAsync} from './basketSlice';
 
 
 const BasketPage = () => {
 
-    const {basket}= useAppSelector(state=> state.basket)
+    const {basket,status}= useAppSelector(state=> state.basket)
     const dispatch=useAppDispatch()
-    const [status, setStatus]=useState({
 
-          loading:false,
-          name:''
-    })
-
-    const handleAddItem=(productId:number, name:string)=>{
-
-        setStatus({loading:true, name})
-        agent.Basket.addItem(productId)
-              .then(basket=>dispatch(setBasket(basket)))
-              .catch(error=>console.log(error))
-              .finally(()=>setStatus({loading:false, name:''}))
-
-    }
-  
-    const handleRemoveItem=(productId:number, quantity=1, name:string)=>{
-
-      setStatus({loading:true, name})
-          agent.Basket.removeItem(productId, quantity)
-                      .then(()=>dispatch(removeItem({productId, quantity})))
-                      .catch(error=>console.log(error))
-                      .finally(()=>setStatus({loading:false, name:''}))
-
-    }
+   
     if (!basket) return <Typography variant='h3'>Your basket is empty</Typography>
     return (
         <>
@@ -69,22 +46,23 @@ const BasketPage = () => {
               <TableCell align="right"> ${(item.price.toFixed(2))}</TableCell>
               <TableCell align="center">
                 <LoadingButton color='error'
-                     loading={status.loading && status.name ==='rem' + item.productId}  
-                     onClick={()=>handleRemoveItem(item.productId,1,'rem' + item.productId)}>
+                     loading={status==='pendingRemoveItem' + item.productId +'rem'}  
+                     onClick={()=>dispatch(removeBasketItemAsync({productId:item.productId, quantity:1, name:'rem'}))}>
                     <Remove/>
                 </LoadingButton>
                 {item.quantity}
                 <LoadingButton color='secondary' 
-                    loading={status.loading && status.name==='add' + item.productId}
-                           onClick={()=>handleAddItem(item.productId, 'add' + item.productId)}>
+                    loading={status==='pendingAddItem' + item.productId}
+                           onClick={()=>dispatch(addBasketItemAsync({productId:item.productId}))}>
                     <Add />
                 </LoadingButton>
                 </TableCell>
               <TableCell align="right">${(item.price *item.quantity).toFixed(2)}</TableCell>
               <TableCell align="right">
                   <LoadingButton color='error' 
-                  loading={status.loading && status.name ==='del' + item.productId}  
-                  onClick={()=>handleRemoveItem(item.productId, item.quantity,'del' + item.productId,)}>
+                  loading={status==='pendingRemoveItem' + item.productId + 'del'}  
+                  onClick={()=>dispatch(removeBasketItemAsync({
+                                        productId:item.productId, quantity:item.quantity, name: 'del'}))}>
                       <Delete/>
                   </LoadingButton>
               </TableCell>
