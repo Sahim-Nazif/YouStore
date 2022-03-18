a2094654895789ca0c3d4e6f7179f36d26cb7e8c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-03-17 20:31:02 -0400

    updated dbInitializer with dummy accounts

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using API.Data;
+using API.Entities;
 using API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,6 +35,11 @@ namespace API
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddCors();
+            services.AddIdentityCore<User>()
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<StoreContext>();
+            services.AddAuthentication();
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
